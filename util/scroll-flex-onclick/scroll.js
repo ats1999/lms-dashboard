86c9d3fd2bf33280d05f10a6f66bc3feb83ab076
ats1999 <rkt82224@gmail.com>
2021-04-27 12:04:15 +0530

    fixed onclick scroll

--- util/scroll-flex-onclick/scroll.js
+++ util/scroll-flex-onclick/scroll.js
@@ -1,11 +1,11 @@
 function scrollLeft(el){
     if(!el) return;
-    el.scrollLeft = el.scrollLeft+200;
+    el.scrollLeft = el.scrollLeft-200;
 }
 
 function scrollRight(el){
     if(!el) return;
-    el.scrollLeft = el.scrollLeft-200;
+    el.scrollLeft = el.scrollLeft+200;
 }
 
 export default {
